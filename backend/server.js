991f6337d3c9b7b435affa1651e9c4eb37992e6b
bamidele <devbammy@gmail.com>
2026-07-09 21:46:10 +0100

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -27,12 +27,18 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 
 app.use(morgan('dev'));
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(
+  cors({
+    origin:
+      process.env.CORS_ORIGIN ||
+      'http://vostainventory.com.ng' ||
+      'https://vosta-global-inventory-system.vercel.app' ||
+      'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+);
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
